653c588c548482894a714f4cb17f954aa6989c2e
taqwa <trushdan@gmail.com>
2018-05-09 17:15:20 -0400

    API get route working.

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // Serve up static assets
 app.use(express.static("client/build"));
+
 // Add routes, both API and view
 app.use(routes);
 
@@ -31,9 +33,9 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
