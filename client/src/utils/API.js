b39553efe0b23c3194223e8441ef9a752aafcc68
Maria <misat0@MISAT0.local>
2018-05-09 13:41:00 -0400

    express addition and animation

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,13 +3,18 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomDog: function() {
+  getRandomUser: function() {
     return axios.get("https://dog.ceo/api/breeds/image/random");
+    // return axios.get("https://dog.ceo/api/breeds/image/random");
   },
-  getDogsOfBreed: function(breed) {
+  getUsersOfSkill: function(breed) {
     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+    // return axios.get("https://dog.ceo/api/breed/" + skill + "/images");
+
   },
   getBaseBreedsList: function() {
     return axios.get("https://dog.ceo/api/breeds/list");
+    // return axios.get("https://dog.ceo/api/breeds/list");
+
   }
 };
