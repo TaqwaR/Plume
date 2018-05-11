ad8475bbfbc8a2e5861f1c9127fbdb19c5a8c7de
Taqwa <trushdan@gmail.com>
2018-05-11 15:30:45 -0400

    Update SearchResults.js

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -5,7 +5,7 @@ const SearchResults = props => (
   <ul className="list-group search-results">
     {props.results.map(result => (
       <li key={result} className="list-group-item">
-        <img alt="lolz" src={result} className="img-responsive" />
+        <img alt="Skill" src={result} className="img-responsive" />
       </li>
     ))}
   </ul>
