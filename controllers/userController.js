3b7d257af3b24ca1d0c222f10d812b7741ccbf66
taqwa <trushdan@gmail.com>
2018-05-10 20:31:42 -0400

    working on search by skill function

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,5 +33,11 @@ module.exports = {
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  getSkills: function(req, res) {
+    db.User
+      .findById({ _id: req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   }
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
 // Matches with "/api/users"
-router.route("/profile/skill")
+router.route("/")
   .get(userController.findAll)
   .post(userController.create);
 
@@ -12,5 +12,10 @@ router
   .get(userController.findById)
   .put(userController.update)
   .delete(userController.remove);
+
+// Return users with searched skill
+router
+  .route("/skill")
+  .get(userController.getSkills);
 
 module.exports = router;
