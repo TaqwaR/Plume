f83f6bdfea54baafc7b25b05e3100a72f31adbf5
taqwa <trushdan@gmail.com>
2018-05-11 16:32:21 -0400

    small changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,10 +6,10 @@ export default {
   getRandomUser: function() {
     return axios.get("api/users/profile");
   },
-  getSkill: function(skill) {
-    return axios.get("/api/users/profile/" + skill + "");
+  getUsersOfSkill: function(skill) {
+    return axios.get("/api/users/skills/" + skill);
   },
   getBaseSkillsList: function() {
-    return axios.get("/api/users/skills");
+    return axios.get("/api/users/");
   }
 };
